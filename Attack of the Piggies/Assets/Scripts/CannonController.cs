caae3bd04c58681874d370e73944396356af4a02
stolenlegos <stolenlegos@gmail.com>
2021-09-19 19:14:12 -0400

    updated piggies

--- Attack of the Piggies/Assets/Scripts/CannonController.cs
+++ Attack of the Piggies/Assets/Scripts/CannonController.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class CannonController : MonoBehaviour
 {
-  public Camera camera;
+  public Camera mainCam;
   public Rigidbody2D piggie;
+  private float angleOfCannon;
+  private float MAX_ANGLE = 80f;
+  private float MIN_ANGLE = 10f;
 
     void Start() {
       piggie.gravityScale = 0;
     }
 
     void Update() {
-      Vector3 pointInWorld = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -camera.transform.position.z);
+      Vector3 pointInWorld = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -mainCam.GetComponent<Camera>().transform.position.z);
+      Vector3 mouseInWorld = mainCam.GetComponent<Camera>().ScreenToWorldPoint(pointInWorld);
+      Vector3 direction = mouseInWorld - transform.position;
+      angleOfCannon = Vector3.Angle(direction, Vector3.right);
 
-      Vector3 mouseInWorld = camera.ScreenToWorldPoint(pointInWorld);
+      //Debug.Log(direction);
+      Debug.Log(angleOfCannon);
 
-      transform.LookAt(mouseInWorld);
+      if (angleOfCannon <= MAX_ANGLE && angleOfCannon > MIN_ANGLE && direction.y > 0) {
+        transform.LookAt(mouseInWorld);
+      }
 
       if(Input.GetMouseButton(0)) {
         piggie.gravityScale = 1;
@@ -24,7 +33,7 @@ public class CannonController : MonoBehaviour
 
         Vector3 temp = mouseInWorld - transform.position;
         Vector2 directionAndMagnitudeOfForce = new Vector2(temp.x, temp.y);
-        piggie.AddForce(directionAndMagnitudeOfForce * 2);
+        piggie.AddForce(directionAndMagnitudeOfForce * 1.5f);
       }
     }
 }
