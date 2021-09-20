c2fd97ee1bc87c0275d4c00fba654faf66a612af
stolenlegos <stolenlegos@gmail.com>
2021-09-19 22:04:48 -0400

    updated piggies
    can now instantiate repeatedly.

---
+++ Attack of the Piggies/Assets/Scripts/CannonSystem.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonSystem : MonoBehaviour, Observer
+{
+  public Camera mainCam;
+  private float angleOfCannon;
+  private float MAX_ANGLE = 80f;
+  private float MIN_ANGLE = 10f;
+  public GameObject pigPrefab;
+
+    void Start() {
+      foreach (SubjectBeingObserved subject in FindObjectsOfType<SubjectBeingObserved>()) {
+        subject.AddObserver(this);
+      }
+    }
+
+
+    void Update() {
+      Vector3 pointInWorld = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -mainCam.GetComponent<Camera>().transform.position.z);
+      Vector3 mouseInWorld = mainCam.GetComponent<Camera>().ScreenToWorldPoint(pointInWorld);
+      Vector3 direction = mouseInWorld - transform.position;
+      angleOfCannon = Vector3.Angle(direction, Vector3.right);
+
+      if (angleOfCannon <= MAX_ANGLE && angleOfCannon > MIN_ANGLE && direction.y > 0) {
+        transform.LookAt(mouseInWorld);
+      }
+    }
+
+
+    public void OnNotify(object obj, NotificationType notificationType) {
+      if (notificationType == NotificationType.PiggieFired) {
+        Debug.Log("Notified");
+        GameObject newPig = Instantiate(pigPrefab);
+      }
+    }
+}
---
+++ Attack of the Piggies/Assets/Scripts/PiggieScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiggieScript : SubjectBeingObserved
+{
+  private Camera mainCam;
+  private Rigidbody2D rb2d;
+  private Transform cannonSprite;
+  private CircleCollider2D col;
+  private bool notDone;
+
+    void Start()
+    {
+      mainCam = GameObject.Find("Camera").GetComponent<Camera>();
+
+      cannonSprite = GameObject.Find("CannonSprite").GetComponent<Transform>();
+      transform.SetParent(cannonSprite, true);
+      transform.localPosition = new Vector3 (1.467542f, 0.4665815f, 0);
+
+      col = GetComponent<CircleCollider2D>();
+      col.enabled = false;
+
+      rb2d = GetComponent<Rigidbody2D>();
+      rb2d.gravityScale = 0;
+
+      notDone = true;
+    }
+
+
+    void Update()
+    {
+      Vector3 pointInWorld = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -mainCam.GetComponent<Camera>().transform.position.z);
+      Vector3 mouseInWorld = mainCam.GetComponent<Camera>().ScreenToWorldPoint(pointInWorld);
+
+      if(Input.GetMouseButtonDown(0)) {
+        Notify(this, NotificationType.PiggieFired);
+      }
+
+      if(Input.GetMouseButtonDown(0) && notDone) {
+        rb2d.gravityScale = 1;
+        transform.parent = null;
+        col.enabled = true;
+
+        Vector3 direction = mouseInWorld - transform.position;
+        Vector2 directionAndMagnitudeOfForce = new Vector2(direction.x, direction.y);
+        rb2d.AddForce(directionAndMagnitudeOfForce * 75f);
+
+        Debug.Log("Fired");
+        notDone= false;
+      }
+    }
+}
